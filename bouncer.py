c21d82abb544b126bed5fdc74ab9d0103c0a70ab
Robyn Kay Baranowski <rkbaranow@gmail.com>
2025-09-30 17:06:39 -0500

    Created and modified code along exercise for Section 8 of Colt Steele Python3 course

--- bouncer.py
+++ bouncer.py
@@ -1,13 +1,28 @@
 #ask for age
-age = input("How old are you? ")
+#age = input("How old are you? ")
+#if age:
+#    age = int(age)
+#    if age >= 18 and age < 21:
+#        print("You can enter, but you need a wristband!")
+#    # 18-21 wristband
+#    #21+ drink, normal entry
+#    elif age >= 21:
+#        print("You can enter and drink!!")
+#    # too young, sorry
+#    else: 
+#        print("You are not old enough to enter!")   
+#else: 
+#    print("Please enter your age to continue")  
 
-if age >= "18" and age < "21":
-    print("You can enter, but you need a wristband!")
-# 18-21 wristband
-#21+ drink, normal entry
-elif int(age) >= 21:
-    print("You are good to go!!")
-# too young, sorry
+
+age = input("How old are you? ")
+if age:
+    age = int(age)
+    if age >= 21:
+        print("You are good to enter and drink!")
+    elif age >= 18:
+        print("You can enter, but you need a wristband!")
+    else: 
+        print("You are not old enough to enter!")   
 else: 
-    print("You are not old enough to enter!")   
-    
+    print("Please enter your age to continue")  
